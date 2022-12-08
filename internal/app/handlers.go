bbccad3856bb2eea601945fbcbbeb58b247d3910
bbt-t <zlukcss@gmail.com>
2022-12-08 12:41:03 +0700

    sprint1 -> v3.3_alpha

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -1,16 +1,15 @@
 package app
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/bbt-t/shortenerURL/configs"
+	"github.com/bbt-t/shortenerURL/pkg"
+	"github.com/go-chi/chi/v5"
 	"io"
 	"log"
 	"net/http"
-
-	"github.com/bbt-t/shortenerURL/configs"
-	"github.com/bbt-t/shortenerURL/pkg"
-
-	"github.com/go-chi/chi/v5"
+	"net/url"
+	"strings"
 )
 
 func (h *ServerHandler) redirectToOriginalURL(w http.ResponseWriter, r *http.Request) {
@@ -25,7 +24,8 @@ func (h *ServerHandler) redirectToOriginalURL(w http.ResponseWriter, r *http.Req
 		http.NotFound(w, r)
 	} else {
 		w.Header().Set("Location", originalURL)
-		http.Redirect(w, r, originalURL, http.StatusTemporaryRedirect)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusTemporaryRedirect)
 	}
 }
 
@@ -35,34 +35,42 @@ func (h *ServerHandler) takeAndSendURL(w http.ResponseWriter, r *http.Request) {
 		Received, run through the HASH-func and write (hash, original url)
 		to the DB and (hash only) response Body, sent response.
 	*/
-	var shortURLRequest ResShortURL
-	var shortURL []byte
 	cfg := configs.NewConfServ()
 
 	defer r.Body.Close()
-	payload, err := io.ReadAll(r.Body)
+	payload, _ := io.ReadAll(r.Body)
+	query, err := url.ParseQuery(string(payload))
 	if err != nil {
+		http.Error(
+			w,
+			fmt.Sprintf("Incorrent request body: %s", payload),
+			http.StatusBadRequest,
+		)
+		return
+	}
+	originalURL := strings.TrimSpace(query.Get("url")) // remove spaces
+	if originalURL == "" {
+		originalURL = string(payload)
+	}
+	//if !pkg.URLValidation(originalURL) {
+	//	http.Error(
+	//		w,
+	//		fmt.Sprintf("Incorrent URL: %s", payload),
+	//		http.StatusBadRequest,
+	//	)
+	//}
+	hashedVal := fmt.Sprintf("%d", pkg.HashShortening([]byte(originalURL)))
+
+	if err := h.store.SaveURL(hashedVal, originalURL); err != nil {
 		log.Printf("ERROR : %s", err)
 	}
-	if err := json.Unmarshal(payload, &shortURLRequest); err != nil {
-		log.Printf("ERROR: %s", err)
-	}
 
-	if pkg.URLValidation(shortURLRequest.URL) {
-		hashedVal := fmt.Sprintf("%d", pkg.HashShortening([]byte(shortURLRequest.URL)))
-
-		if err := h.store.SaveURL(hashedVal, shortURLRequest.URL); err != nil {
-			log.Printf("ERROR : %s", err)
-		}
-
-		shortURL = []byte(
-			fmt.Sprintf(
-				"http://%s:%s/%s", cfg.ServerAddress, cfg.Port, hashedVal),
-		)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusCreated)
+	shortURL := []byte(
+		fmt.Sprintf(
+			"http://%v:%v/%v", cfg.ServerAddress, cfg.Port, hashedVal),
+	)
 	if _, err := w.Write(shortURL); err != nil {
 		log.Printf("ERROR : %s", err)
 	}
---
+++ internal/app/handlers_test.go
@@ -0,0 +1,93 @@
+package app
+
+import (
+	"github.com/bbt-t/shortenerURL/internal/app/storage"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type Fields struct {
+	urls map[string]string
+}
+
+func (f Fields) TestHandler_takeAndSendURL(t *testing.T) {
+	type want struct {
+		code int
+		url  string
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "test POST #1",
+			want: want{
+				code: 201,
+				url:  "https://ya.ru",
+			},
+		},
+	}
+	for _, tt := range tests {
+		for t.Run(tt.name, func(t *testing.T) {}) {
+			request := httptest.NewRequest(http.MethodPost, "/", nil)
+
+			w := httptest.NewRecorder()
+			db := storage.NewMapDBPlug()
+			th := NewHandlerServer(db)
+
+			appH := http.HandlerFunc(th.takeAndSendURL)
+			appH.ServeHTTP(w, request)
+			res := w.Result()
+			if res.StatusCode != tt.want.code {
+				t.Errorf("Exepted status code %d, got %d", tt.want.code, w.Code)
+			}
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatal()
+			}
+			shortURL := string(resBody)[len("http://localhost:8080/"):]
+			f.urls[tt.want.url] = shortURL
+		}
+	}
+}
+
+func (f Fields) TestHandler_redirectToOriginalURL(t *testing.T) {
+	type want struct {
+		code     int
+		location string // f.
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "test GET #1",
+			want: want{
+				code:     307,
+				location: "https://ya.ru",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, "/"+tt.want.location, nil)
+			w := httptest.NewRecorder()
+			db := storage.NewMapDBPlug()
+			th := NewHandlerServer(db)
+			appH := http.HandlerFunc(th.redirectToOriginalURL)
+			appH.ServeHTTP(w, request)
+			res := w.Result()
+			defer res.Body.Close()
+			if res.StatusCode != tt.want.code {
+				t.Errorf("Expected status code %d, got %d", tt.want.code, w.Code)
+			}
+			resLocation := res.Header.Get("Location")
+			if resLocation != tt.want.location {
+				t.Errorf("Expected location %s, got %s", tt.want.location, resLocation)
+			}
+		})
+	}
+}
