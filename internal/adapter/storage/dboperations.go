b2074f8193f6ca42081508323e6853e0f4511394
foagdude <nk-again@rambler.ru>
2023-01-23 18:11:56 +0700

    sprint4 inc14 ?

--- internal/adapter/storage/dboperations.go
+++ internal/adapter/storage/dboperations.go
@@ -167,6 +167,7 @@ func saveURLBatch(ctx context.Context, db *sqlx.DB, uid uuid.UUID, urlBatch []en
 	return nil
 }
 
+// ////// с такой проходит 1/3 14inc /////////////
 func deleteURLArray(db *sqlx.DB, uid uuid.UUID, inpJSON []byte) error {
 	inpURLs := pkg.ConvertStrToSlice(string(inpJSON))
 	qtx := "UPDATE items SET deleted=true WHERE user_id=$1 AND short_url=$2 returning id"
@@ -196,12 +197,15 @@ func deleteURLArray(db *sqlx.DB, uid uuid.UUID, inpJSON []byte) error {
 	return nil
 }
 
+////////////////////////////////////////////////////////
+
 type MustUpdate struct {
 	Stmt   *sqlx.Stmt
 	UserID uuid.UUID
 	InpURL string
 }
 
+// ////// с такой вообще не проходит 14inc //////////////
 func deleteURLArrayFanIn(db *sqlx.DB, uid uuid.UUID, inpJSON []byte) error {
 
 	inpURLs := pkg.ConvertStrToSlice(string(inpJSON))
@@ -276,3 +280,5 @@ func sq(in <-chan MustUpdate) <-chan bool {
 	}()
 	return out
 }
+
+/////////////////////////////////////////////////
